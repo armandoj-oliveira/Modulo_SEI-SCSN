4b9fe13c0afa93779a4f0716ed9987d9693401aa
armandoj-oliveira <ajvdoliveira678512@gmail.com>
2024-11-18 08:06:41 -0300

    update: melhoria no modal

--- modal/analyzeModal.js
+++ modal/analyzeModal.js
@@ -5,7 +5,7 @@ function analyzeModal() {
             <hr style="border: none; height: 3px; background: linear-gradient(to right, #ff7e5f, #feb47b);">
             <br />
             <fieldset style="border: 1px solid #ddd; padding: 20px; border-radius: 8px;">
-                <legend style="font-weight: bold; color: #333;">Selecione uma Modalidade</legend>
+                <legend style="font-weight: bold; color: #333;"> Selecione uma Modalidade </legend>
                 <div style="text-align: left; font-family: Arial, sans-serif; color: #333;">
                     <div style="display: flex; align-items: center; margin-bottom: 12px;">
                         <input type="checkbox" id="inexigibilidade" name="opcoes" value="Inexigibilidade" 
@@ -29,9 +29,12 @@ function analyzeModal() {
                     </div>
                 </div>
             </fieldset>
-            <br />
-            <textarea style="width: 100%; padding: 12px; border: 1px solid #ccc; border-radius: 8px; font-size: 14px; color: #333; background-color: #f9f9f9; 
-                resize: vertical; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); transition: all 0.3s ease;">TESTE</textarea>
+            <div style="background-color: #f0f0f0; padding: 20px; border-left: 5px solid #007acc; border-radius: 4px; margin-top: 20px; box-sizing: border-box;">
+                <h3 style="margin: 0; color: #333; font-size: 1.25em;">Informações Adicionais:</h3>
+                <textarea style="width: calc(100% - 24px); margin-top: 10px; padding: 12px; border: 1px solid #ccc; border-radius: 8px; font-size: 14px; color: #333; 
+                    background-color: #f9f9f9; resize: vertical; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); transition: all 0.3s ease;" 
+                    placeholder="Insira as informações aqui"></textarea>
+            </div>
             <br />
             <div style="display: flex; justify-content: center; align-items: center; gap: 20px;">
                 <button id="btnContinue" style="padding: 10px 20px; background-color: #4CAF50; border: none; border-radius: 8px; color: white; font-weight: bold;
